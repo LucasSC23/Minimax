14bded543b82090944d18f3f2c477055856abfa7
Lucas Sostoa <Lucassostoa.2002@gmail.com>
2026-03-31 09:58:21 -0300

    Cambio de nombre de clase a ClasesCatMouse/ Correccion de error de impresion

--- Principal.py
+++ Principal.py
@@ -1,7 +1,7 @@
 from tablero import Tablero #Importo mi tablero de la clase tablero
-from clases import mouse, cat
+from ClasesCatMouse import mouse, cat
 
-mi_tablero=Tablero(5,5)#Dimensionamos nuestro tablero
+mi_tablero=Tablero(6,6)#Dimensionamos nuestro tablero
 mi_gato= cat(0,0)#Posicionamos al gato en su ubicacion inicial
 mi_raton=mouse(4,4)#Posicionamos al raton en su ubicacion inicial
 
@@ -19,8 +19,8 @@ while True:
     
 
     #Colocamos al gato y al raton dentro de la matriz
-    mi_tablero.matriz=[[mi_gato.columna][mi_gato.fila]]= 'G'
-    mi_tablero.matriz=[[mi_raton.columna][mi_raton.fila]]= 'R'
+    mi_tablero.matriz[mi_gato.columna][mi_gato.fila]= 'G'
+    mi_tablero.matriz[mi_raton.columna][mi_raton.fila]= 'R'
     mi_tablero.dimension()
     
     if mi_gato.atrapa(mi_raton):#Implementamos el metodo atrapa en el main
--- tablero.py
+++ tablero.py
@@ -2,11 +2,11 @@ class Tablero:
     def __init__(self,filatab,columnatab):
         self.filatab=filatab
         self.columnatab=columnatab
-        self.matriz=[['.' for _ in range (columnatab)] for _ in range (filatab)]
+        self.matriz=[['-' for _ in range (columnatab)] for _ in range (filatab)]
     def dimension(self):#Imprimimos la matriz
         print("\n----Tablero---")
         for filatab in self.matriz:
-            print(" [ " + "  " .join(filatab)+ " ] ")
+            print(" [ " + "   " .join(filatab) + " ] ")
             
 
         
